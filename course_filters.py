c9245d298f01d8c50b455bb14e8bb42ee5f598ec
Aaron Jimenez <roceaaan@gmail.com>
2020-06-19 17:24:35 -0700

    Completed filter, just need counts

---
+++ course_filters.py
@@ -0,0 +1,212 @@
+from course_values import *
+
+
+# Function Selector
+def course_filter_selector(course_name):
+    if "500" in course_name:
+        return course_500
+    elif "502" in course_name:
+        return course_502
+    elif "504" in course_name:
+        return course_504
+    elif "506" in course_name:
+        return course_506
+    elif "508" in course_name:
+        return course_508
+    elif "510" in course_name:
+        return course_510
+    elif "520" in course_name:
+        return course_520
+    elif "521" in course_name:
+        return course_521
+    elif "523" in course_name:
+        return course_523
+    elif "524" in course_name:
+        return course_524
+    elif "525" in course_name:
+        return course_525
+    elif "526" in course_name:
+        return course_526
+    elif "530" in course_name:
+        return course_530
+    elif "531" in course_name:
+        return course_531
+    elif "540" in course_name:
+        return course_540
+    elif "541" in course_name:
+        return course_541
+    elif "545" in course_name:
+        return course_545
+    elif "550" in course_name:
+        return course_550
+    elif "555" in course_name:
+        return course_555
+    elif "560" in course_name:
+        return course_560
+    elif "570" in course_name:
+        return course_570
+    elif "572" in course_name:
+        return course_572
+    else:
+        return allow_all
+
+
+def allow_all(student_classes):
+    return True
+
+
+# Foundation Course Functions
+
+def course_500(student_classes):
+    # print("Name: "+student_classes[1])
+    # print(student_classes[B500])
+    return student_classes[B500] is None
+
+
+def course_502(student_classes):
+    return student_classes[B502] is None
+
+
+def course_504(student_classes):
+    return student_classes[B504] is None
+
+
+def course_506(student_classes):
+    return student_classes[B506] is None
+
+
+def course_508(student_classes):
+    return student_classes[B508] is None
+
+
+def taken_all_foundation(student_classes):
+    if course_500(student_classes) is not True and course_502(student_classes) is not True and course_504(
+            student_classes) is not True and course_506(student_classes) is not True and course_508(
+        student_classes) is not True:
+        return True
+    else:
+        return False
+
+
+# Core Course Functions
+
+def count_core_courses(student_classes):
+    core_count = 0
+    for i in range(B510, B572):
+        if student_classes[i] is not None:
+            core_count += student_classes[i]
+
+    return core_count
+
+
+def course_510(student_classes):
+    return student_classes[B510] is None and taken_all_foundation(student_classes)
+
+
+def course_520(student_classes):
+    return student_classes[B520] is None and taken_all_foundation(student_classes)
+
+
+def course_530(student_classes):
+    return taken_all_foundation(student_classes) and (student_classes[B530] is None) and (
+            student_classes[B510] is not None or student_classes[B520] is not None) and (
+                   count_core_courses(student_classes) < 10)
+
+
+def course_540(student_classes):
+    return taken_all_foundation(student_classes) and (student_classes[B540] is None) and (
+            student_classes[B510] is not None or student_classes[B520] is not None) and (
+                   count_core_courses(student_classes) < 10)
+
+
+def course_545(student_classes):
+    return taken_all_foundation(student_classes) and (student_classes[B545] is None) and (
+            student_classes[B510] is not None or student_classes[B520] is not None) and (
+                   count_core_courses(student_classes) < 10)
+
+
+def course_550(student_classes):
+    return taken_all_foundation(student_classes) and (student_classes[B550] is None) and (
+            student_classes[B510] is not None or student_classes[B520] is not None) and (
+                   count_core_courses(student_classes) < 10)
+
+
+def course_555(student_classes):
+    return taken_all_foundation(student_classes) and (student_classes[B555] is None) and (
+            student_classes[B510] is not None or student_classes[B520] is not None) and (
+                   count_core_courses(student_classes) < 10)
+
+
+def course_560(student_classes):
+    return taken_all_foundation(student_classes) and (student_classes[B560] is None) and (
+            student_classes[B510] is not None or student_classes[B520] is not None) and (
+                   count_core_courses(student_classes) < 10)
+
+
+def course_570(student_classes):
+    core_count = count_core_courses(student_classes)
+
+    # Temporary
+    return (student_classes[B510] is not None) and (student_classes[B520] is not None) and (
+                student_classes[B530] is not None) and (student_classes[B540] is not None) and (
+                       student_classes[B550] is not None) and (student_classes[B560] is not None) and (
+                       student_classes[B570] is None) and (student_classes[3] == "MBA") and (core_count >= 7) and (
+                       core_count < 10)
+
+    return ((core_count >= 7) and (core_count < 10)) and student_classes[B570] is None
+
+
+# Special Topics Functions
+
+def course_521(student_classes):
+    return calculate_eligible_for_st(student_classes)
+
+
+def course_523(student_classes):
+    return calculate_eligible_for_st(student_classes)
+
+
+def course_524(student_classes):
+    return calculate_eligible_for_st(student_classes)
+
+
+def course_525(student_classes):
+    return calculate_eligible_for_st(student_classes)
+
+
+def course_526(student_classes):
+    return calculate_eligible_for_st(student_classes)
+
+
+def course_531(student_classes):
+    return calculate_eligible_for_st(student_classes)
+
+
+def course_541(student_classes):
+    return calculate_eligible_for_st(student_classes)
+
+
+def course_572(student_classes):
+    return taken_all_foundation(student_classes) and (count_core_courses(student_classes) >= 2)
+
+
+def calculate_eligible_for_st(student_classes):
+    return (student_classes[B510] is not None and student_classes[B520] is not None) and (
+            calculate_total_special_topics(student_classes) < 3)
+
+
+def calculate_total_special_topics(student_classes):
+    total_st = 0
+    for i in range(B521, B530):
+        if student_classes[i] is not None:
+            total_st += student_classes[i]
+
+    # Check for 531
+    if student_classes[B531] is not None:
+        total_st += student_classes[B531]
+
+    # Check for 541
+    if student_classes[B531] is not None:
+        total_st += student_classes[B541]
+
+    return total_st
--- mba_database_parser.py
+++ mba_database_parser.py
@@ -2,6 +2,8 @@ from openpyxl import *
 import load_and_save_file_dialog
 from openpyxl.styles import Font
 from copy import copy
+import course_values
+import course_filters
 
 
 class Spreadsheet:
@@ -10,6 +12,8 @@ class Spreadsheet:
         self.workbook = load_workbook(filename=self.fileName)
 
         self.setWorksheets()
+
+        self.blankCellStyle = self.Style(self.main_worksheet.cell(row=222, column=1))
 
         self.clipboard = []
 
@@ -32,7 +36,9 @@ class Spreadsheet:
         pass
 
     def saveWorkbook(self):
+        print("Saving...")
         self.workbook.save(filename=load_and_save_file_dialog.getSaveDir())
+        print("Saved")
 
     def setWorksheets(self):
         worksheets = self.workbook.worksheets
@@ -60,8 +66,9 @@ class Spreadsheet:
         copy_core_course_names.clear()
 
         # Create dictionaries for each course with worksheet
-        self.foundation_courses = {foundation_courses_names[i]: self.FoundationWorksheet(self, foundation_courses_names[i]) for i
-                                   in range(0, len(foundation_courses_names))}
+        self.foundation_courses = {
+            foundation_courses_names[i]: self.FoundationWorksheet(self, foundation_courses_names[i]) for i
+            in range(0, len(foundation_courses_names))}
 
         self.core_courses = {core_courses_names[i]: self.CoreWorksheet(self, core_courses_names[i]) for i in
                              range(0, len(core_courses_names))}
@@ -81,7 +88,7 @@ class Spreadsheet:
             fontRow = []
             for j in range(self.table_startingCol, self.table_endCol + 1, 1):
                 rowSelected.append(self.main_worksheet.cell(row=i, column=j).value)
-                fontRow.append(self.Style(self.main_worksheet.cell(row=i,column=j)))
+                fontRow.append(self.Style(self.main_worksheet.cell(row=i, column=j)))
             # Adds the RowSelected List and nests inside the rangeSelected
             rangeSelected.append(rowSelected)
             fontRange.append(fontRow)
@@ -90,20 +97,27 @@ class Spreadsheet:
 
     def copyClipboardToAllWS(self):
         for course in self.foundation_courses:
-            self.foundation_courses[course].pasteRange(self.table_startingCol, self.table_startingRow, self.table_endCol, self.table_endRow)
+            self.foundation_courses[course].clearRange(self.table_startingCol, self.table_startingRow,
+                                                       self.table_endCol, self.table_endRow)
+            self.foundation_courses[course].pasteRange(self.table_startingCol, self.table_startingRow,
+                                                       self.table_endCol, self.table_endRow)
             self.foundation_courses[course].calculateCounts()
 
-
         for course in self.core_courses:
-            self.core_courses[course].pasteRange(self.table_startingCol, self.table_startingRow, self.table_endCol, self.table_endRow)
+            self.core_courses[course].clearRange(self.table_startingCol, self.table_startingRow,
+                                                 self.table_endCol, self.table_endRow)
+            self.core_courses[course].pasteRange(self.table_startingCol, self.table_startingRow, self.table_endCol,
+                                                 self.table_endRow)
             self.core_courses[course].calculateCounts()
 
         for course in self.special_topics_courses:
-            self.special_topics_courses[course].pasteRange(self.table_startingCol, self.table_startingRow, self.table_endCol, self.table_endRow)
+            self.special_topics_courses[course].clearRange(self.table_startingCol, self.table_startingRow,
+                                                           self.table_endCol, self.table_endRow)
+            self.special_topics_courses[course].pasteRange(self.table_startingCol, self.table_startingRow,
+                                                           self.table_endCol, self.table_endRow)
             self.special_topics_courses[course].calculateCounts()
 
         pass
-
 
     def findEndRow(self):
         row = 3
@@ -130,7 +144,7 @@ class Spreadsheet:
         # Find start row for MBA
         while (self.main_worksheet.cell(row=row,
                                         column=col).value is not None) and "MBA" not in self.main_worksheet.cell(
-                row=row, column=col).value:
+            row=row, column=col).value:
             row += 1
 
         self.MBA_start_row = row
@@ -150,7 +164,7 @@ class Spreadsheet:
         # Find start row for Bus Cert
         while (self.main_worksheet.cell(row=row,
                                         column=col).value is not None) and "Bus Certificate" not in self.main_worksheet.cell(
-                row=row, column=col).value:
+            row=row, column=col).value:
             row += 1
 
         self.Bus_Cert_start_row = row
@@ -158,7 +172,7 @@ class Spreadsheet:
         # Find end row for MBA
         while (self.main_worksheet.cell(row=row,
                                         column=col).value is not None) and "Bus Certificate" in self.main_worksheet.cell(
-                row=row, column=col).value:
+            row=row, column=col).value:
             row += 1
 
         row -= 1
@@ -190,6 +204,8 @@ class Spreadsheet:
         def __init__(self, current_spreadsheet, current_worksheet_name):
             self.current_spreadsheet = current_spreadsheet
             self.current_worksheet = current_spreadsheet.workbook[current_worksheet_name]
+            self.current_worksheet_name = current_worksheet_name
+            self.filter_function = course_filters.course_filter_selector(self.current_worksheet_name)
 
         def copyRange(self, startRow, startCol, endRow, endCol):
             rangeSelected = []
@@ -206,29 +222,48 @@ class Spreadsheet:
             self.current_spreadsheet.clipboard = rangeSelected
 
         def pasteRange(self, startCol, startRow, endCol, endRow):
+            print(self.current_worksheet_name)
             countRow = 0
+            countSheetRow = startRow
             for i in range(startRow, endRow + 1, 1):
                 countCol = 0
-                for j in range(startCol, endCol + 1, 1):
-                    self.current_worksheet.cell(row=i, column=j).value = self.current_spreadsheet.clipboard[countRow][
-                        countCol]
-                    self.current_spreadsheet.fontClipboard[countRow][countCol].copyToCell(self.current_worksheet.cell(row=i, column=j))
-                    # self.current_worksheet.cell(row=i, column=j).font = copy(self.current_spreadsheet.fontClipboard[countRow][countCol])
 
-                    countCol += 1
+                # Filter row
+                if self.filter_function(self.current_spreadsheet.clipboard[countRow]) is True:
+                    # print(self.current_spreadsheet.clipboard[countRow][1])
+                    for j in range(startCol, endCol + 1, 1):
+                        self.current_worksheet.cell(row=countSheetRow, column=j).value = \
+                            self.current_spreadsheet.clipboard[countRow][
+                                countCol]
+                        self.current_spreadsheet.fontClipboard[countRow][countCol].copyToCell(
+                            self.current_worksheet.cell(row=countSheetRow, column=j))
+
+                        countCol += 1
+                    countSheetRow += 1
+
                 countRow += 1
+
+        def clearRange(self, startCol, startRow, endCol, endRow):
+            for i in range(startRow, endRow + 1, 1):
+                for j in range(startCol, endCol + 1, 1):
+                    self.current_worksheet.cell(row=i, column=j).value = None
+                    self.clearStyle(self.current_worksheet.cell(row=i, column=j))
+
+        def clearStyle(self, cell):
+            self.current_spreadsheet.blankCellStyle.copyToCell(cell)
 
     class CoreWorksheet(Worksheet):
         def __init__(self, current_spreadsheet, current_worksheet_name):
             super().__init__(current_spreadsheet, current_worksheet_name)
-            self.current_worksheet_name = current_worksheet_name
 
         def calculateCounts(self):
-            MBA_subtotal = "=SUBTOTAL(3, D" + str(self.current_spreadsheet.MBA_start_row) + ":D" + str(self.current_spreadsheet.MBA_end_row) + ")"
+            MBA_subtotal = "=SUBTOTAL(3, D" + str(self.current_spreadsheet.MBA_start_row) + ":D" + str(
+                self.current_spreadsheet.MBA_end_row) + ")"
             self.current_worksheet.cell(row=169, column=4).value = MBA_subtotal
 
             if "570" not in self.current_worksheet_name:
-                DD_subtotal = "=SUBTOTAL(3, D" + str(self.current_spreadsheet.DD_start_row) + ":D" + str(self.current_spreadsheet.DD_end_row) + ")"
+                DD_subtotal = "=SUBTOTAL(3, D" + str(self.current_spreadsheet.DD_start_row) + ":D" + str(
+                    self.current_spreadsheet.DD_end_row) + ")"
                 self.current_worksheet.cell(row=170, column=4).value = DD_subtotal
 
     class FoundationWorksheet(Worksheet):
@@ -236,13 +271,16 @@ class Spreadsheet:
             super().__init__(current_spreadsheet, current_worksheet_name)
 
         def calculateCounts(self):
-            MBA_subtotal = "=SUBTOTAL(3, D" + str(self.current_spreadsheet.MBA_start_row) + ":D" + str(self.current_spreadsheet.MBA_end_row) + ")"
+            MBA_subtotal = "=SUBTOTAL(3, D" + str(self.current_spreadsheet.MBA_start_row) + ":D" + str(
+                self.current_spreadsheet.MBA_end_row) + ")"
             self.current_worksheet.cell(row=169, column=4).value = MBA_subtotal
 
-            DD_subtotal = "=SUBTOTAL(3, D" + str(self.current_spreadsheet.DD_start_row) + ":D" + str(self.current_spreadsheet.DD_end_row) + ")"
+            DD_subtotal = "=SUBTOTAL(3, D" + str(self.current_spreadsheet.DD_start_row) + ":D" + str(
+                self.current_spreadsheet.DD_end_row) + ")"
             self.current_worksheet.cell(row=170, column=4).value = DD_subtotal
 
-            Bus_Cert_subtotal = "=SUBTOTAL(3, D" + str(self.current_spreadsheet.Bus_Cert_start_row) + ":D" + str(self.current_spreadsheet.Bus_Cert_end_row) + ")"
+            Bus_Cert_subtotal = "=SUBTOTAL(3, D" + str(self.current_spreadsheet.Bus_Cert_start_row) + ":D" + str(
+                self.current_spreadsheet.Bus_Cert_end_row) + ")"
             self.current_worksheet.cell(row=171, column=4).value = Bus_Cert_subtotal
 
     class STWorksheet(Worksheet):
@@ -250,7 +288,8 @@ class Spreadsheet:
             super().__init__(current_spreadsheet, current_worksheet_name)
 
         def calculateCounts(self):
-            MBA_subtotal = "=SUBTOTAL(3, D" + str(self.current_spreadsheet.MBA_start_row) + ":D" + str(self.current_spreadsheet.MBA_end_row) + ")"
+            MBA_subtotal = "=SUBTOTAL(3, D" + str(self.current_spreadsheet.MBA_start_row) + ":D" + str(
+                self.current_spreadsheet.MBA_end_row) + ")"
             self.current_worksheet.cell(row=169, column=4).value = MBA_subtotal
 
     class Style:
